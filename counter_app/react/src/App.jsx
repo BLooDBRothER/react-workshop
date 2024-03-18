50720b52c63c7f5000472554c23acc5b318b373b
BLooDBRothER <computertechnology446@gmail.com>
2024-03-18 06:19:44 +0530

    add react counter app

---
+++ counter_app/react/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prev => prev + 1);
+  }
+
+  const decrement = () => {
+    setCount(prev => prev - 1);
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <h2>count is {count}</h2>
+      <div className="card">
+        <div className='btn-wrapper'>
+          <button onClick={increment}>
+            + increment
+          </button>
+          <button onClick={decrement}>
+            - decrement
+          </button>
+        </div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
